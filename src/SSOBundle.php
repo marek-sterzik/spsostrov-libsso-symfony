dace642cff90f9c1d21f0881bb8cc7ce3d622efe
Marek Sterzik <marek.sterzik@gmail.com>
2024-12-02 22:44:58 +0100

    initial commit

---
+++ src/SSOBundle.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace SPSOstrov\SSOBundle;
+
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+
+class SSOBundle extends AbstractBundle
+{
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        /*
+        // load an XML, PHP or YAML file
+        $container->import('../config/services.xml');
+
+        // you can also add or replace parameters and services
+        $container->parameters()
+            ->set('acme_hello.phrase', $config['phrase'])
+        ;
+
+        if ($config['scream']) {
+            $container->services()
+                ->get('acme_hello.printer')
+                    ->class(ScreamingPrinter::class)
+            ;
+        }
+        */
+    }
+}
