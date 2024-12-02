0a33398ea88e9b7a96bc6d5ef6857de70688f7ee
Marek Sterzik <marek.sterzik@gmail.com>
2024-12-02 23:02:47 +0100

    added basic controller

---
+++ src/Controller/SSOLoginController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace SPSOstrov\SSOBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SSOLoginController
+{
+    /**
+     * @Route("/sso-login", name="spsostrov.sso.login")
+     */
+    public function index(): Response
+    {
+        return new Response("sso-login");
+    }
+}
