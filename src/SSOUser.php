b4d89320c25bcb43e705614761b59ba70bf2d340
Marek Sterzik <marek.sterzik@gmail.com>
2024-12-03 00:38:26 +0100

    improve compatibility

--- src/SSOUser.php
+++ src/SSOUser.php
@@ -2,8 +2,22 @@
 
 namespace SPSOstrov\SSOBundle;
 
+use Symfony\Component\Security\Core\User\UserInterface;
 use SPSOstrov\SSO\SSOUser as SSOUserBase;
 
-class SSOUser extends SSOUserBase
+class SSOUser extends SSOUserBase implements UserInterface
 {
+    public function getRoles(): array
+    {
+        return ['ROLE_USER'];
+    }
+
+    public function eraseCredentials(): void
+    {
+    }
+
+    public function getUserIdentifier(): string
+    {
+        return $this->getLogin();
+    }
 }
